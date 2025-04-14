5be818fafdafd0f4bf296d23c59256fc9b17127f
ZiyaSener <senerzi4883_aehit@students.vizja.pl>
2025-04-15 00:23:43 +0300

    Updated Task1.java to match assignment example code

--- src/Task1.java
+++ src/Task1.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        //This is a line comment
         Scanner scanner = new Scanner(System.in);
-        
-        // Get user's first and last name
-        System.out.print("Enter your first name: ");
-        String firstname = scanner.nextLine();
-        System.out.print("Enter your last name: ");
+        /*
+         * This is a block comment
+         * It can contain multiple lines
+         * Just like here
+         */
+        System.out.println("Please enter your surname:");
         String surname = scanner.nextLine();
-        
-        // Print welcome message
-        System.out.println("Welcome " + firstname + " " + surname);
+        System.out.println("Please enter your forename:");
+        String forename = scanner.nextLine();
         
         scanner.close();
+        
+        System.out.println("Welcome " + forename + " " + surname);
     }
 }
